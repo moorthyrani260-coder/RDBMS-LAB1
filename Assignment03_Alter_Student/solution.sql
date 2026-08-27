6999fba80a01de82ec3da0d71a0b66bf7362f717
moorthyrani260-coder <moorthyrani260@gmail.com>
2026-08-27 11:25:12 +0530

    Update solution.sql

--- Assignment03_Alter_Student/solution.sql
+++ Assignment03_Alter_Student/solution.sql
@@ -1,7 +1,8 @@
--- Alter Student table
-
--- Add Email
-
--- Add PhoneNumber
-
--- Display structure
+CREATE TABLE Student (
+    StudentID INT(5) PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10),
+    DepartmentID INT(5),
+    CONSTRAINT fk_department FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)
+);
